845446f87653d21cee3d1981d55c7ad784f33ee0
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-10-20 13:57:15 -0400

    donnees

---
+++ donnees.js
@@ -0,0 +1,134 @@
+var donnees = {
+  cypress: {
+    id_image: 1
+  },
+  sea-mine: {
+    id_image: 2
+  },
+  greenalgae: {
+    id_image: 3
+  },
+  radar: {
+    id_image: 4
+  },
+  corail: {
+    id_image: 5
+  },
+  corail-el: {
+    id_image: 6
+  },
+  meduse: {
+    id_image: 7
+  },
+  bombe: {
+    id_image: 8
+  },
+  feuilles: {
+    id_image: 9
+  },
+  feuilles-el: {
+    id_image: 10
+  },
+  insecte: {
+    id_image: 11
+  },
+  insecte-el: {
+    id_image: 12
+  },
+  drone1: {
+    id_image: 13
+  },
+  insecte-drone3: {
+    id_image: 14
+  },
+  tank: {
+    id_image: 15
+  },
+  tank-bones: {
+    id_image: 16
+  },
+  insecte1: {
+    id_image: 17
+  },
+  insecteailes-el: {
+    id_image: 18
+  },
+  pelle ecano: {
+    id_image: 19
+  },
+  pelle-mecano-bones: {
+    id_image: 20 
+  },
+  sous-marin: {
+    id_image: 21
+  },
+  sous-marin bones: {
+    id_image: 22 
+  },
+  sentinel: {
+    id_image: 23
+  },
+  sentinel-bones: {
+    id_image: 24
+  },
+  blackbird: {
+    id_image: 25
+  },
+  blackbird-bones: {
+    id_image: 26
+  },
+  archae1: {
+    id_image: 27
+  },
+  archae2: {
+    id_image: 28
+  },
+  bacteria1: {
+    id_image: 29
+  },
+  bacteria2: {
+    id_image: 30
+  },
+  bacteria3: {
+    id_image: 31
+  },
+  baleine: {
+    id_image: 32
+  },
+  diatome-el: {
+    id_image: 33
+  },
+  diatome: {
+    id_image: 34
+  },
+  engrenage: {
+    id_image: 35
+  },
+  fern: {
+    id_image: 36
+  },
+  feuillearbre: {
+    id_image: 37
+  },
+  lys: {
+    id_image: 38
+  },
+  mushroom: {
+    id_image: 39
+  },
+  oiseau: {
+    id_image: 40
+  },
+  palm: {
+    id_image: 41
+  },
+  pieuvre: {
+    id_image: 42
+  },
+  tortue: {
+    id_image: 43
+  },
+  giraffe: {
+    id_image: 44
+  }
+} 
