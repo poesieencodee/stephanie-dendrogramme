3e52969b721620efa718185db9c1b521d2b08dbf
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-10-23 14:46:47 -0400

    donnees completes

--- donnees.js
+++ donnees.js
@@ -7,7 +7,7 @@ var donnees = [
   {
     "name": "flare.techno.sea_mine",
     "id_image": 2,
-    "imports": ["flare.eukaryotes.cypress"]
+    "imports": []
   },
   {
     "name": "flare.eukaryotes.greenalgae",
@@ -17,11 +17,206 @@ var donnees = [
   {
     "name": "flare.techno.radar",
     "id_image": 4,
-    "imports": ["flare.eukaryotes.greenalgae"]
+    "imports": []
   },
   {
     "name": "flare.eukaryotes.corall",
     "id_image": 5,
+    "imports": ["flare.hybride.corall-el"]
+  },
+  {
+    "name": "flare.hybride.corall-el",
+    "id_image": 6,
     "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.meduse",
+    "id_image": 7,
+    "imports": ["flare.techno.bombe"]
+  },
+  {
+    "name": "flare.techno.bombe",
+    "id_image": 8,
+    "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.feuilles",
+    "id_image": 9,
+    "imports": ["flare.hybride.feuilles-el"]
+  },
+  {
+    "name": "flare.hybride.feuilles-el",
+    "id_image": 10,
+    "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.insecte",
+    "id_image": 11,
+    "imports": ["flare.hybride.insect-el"]
+  },
+  {
+    "name": "flare.hybride.insect-el",
+    "id_image": 12,
+    "imports": []
+  },
+  {
+    "name": "flare.techno.drone1",
+    "id_image": 13,
+    "imports": ["flare.hybride.insecte-drone3"]
+  },
+  {
+    "name": "flare.hybride.insecte-drone3",
+    "id_image": 14,
+    "imports": []
+  },
+  {
+    "name": "flare.techno.tank",
+    "id_image": 15,
+    "imports": ["flare.hybride.tank-bones"]
+  },
+  {
+    "name": "flare.hybride.tank-bones",
+    "id_image": 16,
+    "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.insecte1",
+    "id_image": 17,
+    "imports": ["flare.hybride.insecteailes-el"]
+  },
+  {
+    "name": "flare.hybride.insecteailes-el",
+    "id_image": 18,
+    "imports": []
+  },
+  {
+    "name": "flare.techno.pelle-mecano",
+    "id_image": 19,
+    "imports": ["flare.hybride.pelle-mecano-bones"]
+  },
+  {
+    "name": "flare.hybride.pelle-mecano-bones",
+    "id_image": 20,
+    "imports": []
+  },
+  {
+    "name": "flare.techno.sous-marin",
+    "id_image": 21,
+    "imports": ["flare.hybride.sous-marin-bones"]
+  },
+  {
+    "name": "flare.hybride.sous-marin-bones",
+    "id_image": 22,
+    "imports": []
+  },
+  {
+    "name": "flare.techno.sentinel",
+    "id_image": 23,
+    "imports": ["flare.hybride.sentinel-bones"]
+  },
+  {
+    "name": "flare.hybride.sentinel-bones",
+    "id_image": 24,
+    "imports": []
+  },
+  {
+    "name": "flare.techno.blackbird",
+    "id_image": 25,
+    "imports": ["flare.hybride.blackbird-bones"]
+  },
+  {
+    "name": "flare.hybride.blackbird-bones",
+    "id_image": 26,
+    "imports": []
+  },
+  {
+    "name": "flare.archae.archae1",
+    "id_image": 27,
+    "imports": []
+  },
+  {
+    "name": "flare.archae.archae2",
+    "id_image": 28,
+    "imports": []
+  },
+  {
+    "name": "flare.bacterie.bacteria1",
+    "id_image": 29,
+    "imports": []
+  },
+  {
+    "name": "flare.bacterie.bacteria2",
+    "id_image": 30,
+    "imports": []
+  },
+  {
+    "name": "flare.bacterie.bacteria3",
+    "id_image": 31,
+    "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.baleine",
+    "id_image": 32,
+    "imports": ["flare.techno.sous-marin", "flare.hybride.sous-marin-bones"]
+  },
+  {
+    "name": "flare.techno.diatome-el",
+    "id_image": 33,
+    "imports": ["flare.eukaryotes.diatome"]
+  },
+  {
+    "name": "flare.eukaryotes.diatome",
+    "id_image": 34,
+    "imports": []
+  },
+  {
+    "name": "flare.techno.engrenage",
+    "id_image": 35,
+    "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.fern",
+    "id_image": 36,
+    "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.feuillearbre",
+    "id_image": 37,
+    "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.lys",
+    "id_image": 38,
+    "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.mushroom",
+    "id_image": 39,
+    "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.oiseau",
+    "id_image": 40,
+    "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.palm",
+    "id_image": 41,
+    "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.pieuvre",
+    "id_image": 42,
+    "imports": []
+  },
+  {
+    "name": "flare.eukaryotes.tortue",
+    "id_image": 43,
+    "imports": ["flare.techno.pelle-mecano", "flare.hybride.pelle-mecano-bones"]
+  },
+  {
+    "name": "flare.eukaryotes.giraffe",
+    "id_image": 44,
+    "imports": ["flare.techno.pelle-mecano", "flare.hybride.pelle-mecano-bones"]
   }
 ]
